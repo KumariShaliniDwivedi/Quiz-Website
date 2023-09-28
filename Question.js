88b02042391e507084c4e55fa9f3d92fff580958
KumariShaliniDwivedi <132951829+KumariShaliniDwivedi@users.noreply.github.com>
2023-09-28 14:50:19 +0530

    Add files via upload

---
+++ Question.js
@@ -0,0 +1,57 @@
+let questions=[
+    {
+        numb:1,
+        question:"What does HTML stand for?",
+        answer:"B. Hyper Text Markup Language",
+        options:[
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Markup Language",
+            "C. Hyper Text Multiple Language",
+            "D. Home Text Multi Language",
+        ]
+    },
+    {
+        numb:2,
+        question:"What does CSS stand for?",
+        answer:"A. Cascading Style Sheet",
+        options:[
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet",
+        ]
+    },
+    {
+        numb:3,
+        question:"What does PHP stand for?",
+        answer:"A. Hypertext Preprocessor",
+        options:[
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor",
+        ]
+    },
+    {
+        numb:4,
+        question:"What does SQL stand for?",
+        answer:"D. Structured Query Language",
+        options:[
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language",
+        ]
+    },
+    {
+        numb:5,
+        question:"What does XML stand for?",
+        answer:"C. Extensible Markup Language",
+        options:[
+            "A. Excess Multiple Language",
+            "B. Excellent Multiple Language",
+            "C. Extensible Markup Language",
+            "D. Explore Multi Language",
+        ]
+    },
+];
